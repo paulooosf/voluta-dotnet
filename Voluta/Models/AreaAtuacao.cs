7811035c30c4357ae4022b644f329a86bf85d538
Paulo Henrique <phpessoal2021@gmail.com>
2025-05-27 23:31:48 -0300

    feat: criando enum de area de atuação

---
+++ Voluta/Models/AreaAtuacao.cs
@@ -0,0 +1,19 @@
+namespace Voluta.Models
+{
+    public enum AreaAtuacao
+    {
+        EducacaoEnsino,
+        SaudeBemEstar,
+        MeioAmbiente,
+        ProtecaoAnimal,
+        AssistenciaSocial,
+        CulturaDiversidade,
+        EsporteLazer,
+        DireitosHumanos,
+        SegurancaAlimentar,
+        DesenvolvimentoComunitario,
+        EmpreendedorismoSocial,
+        TecnologiaInovacao,
+        ArtesCultura
+    }
+} 
