416bc84a4c8364fa4bbed6e0303eb044095dca66
Paulo Henrique <phpessoal2021@gmail.com>
2025-05-27 22:58:42 -0300

    feat: adicionando suporte a paginação

---
+++ Voluta/ViewModels/PaginatedViewModel.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Voluta.ViewModels
+{
+    public class PaginatedViewModel<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalItems { get; set; }
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+    }
+} 
