f6074ddc0e384f5a8959db3a30cf6004ae0447b7
Paulo Henrique <phpessoal2021@gmail.com>
2025-05-28 23:04:14 -0300

    feat: primeira migration

---
+++ Voluta/Migrations/20250528023953_CriacaoInicial.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Voluta.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ongs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Cnpj = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AreasAtuacaoJson = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ongs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Disponivel = table.Column<bool>(type: "bit", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AreasInteresseJson = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SolicitacoesVoluntariado",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false),
+                    OngId = table.Column<int>(type: "int", nullable: false),
+                    DataSolicitacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataAprovacao = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Mensagem = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SolicitacoesVoluntariado", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SolicitacoesVoluntariado_Ongs_OngId",
+                        column: x => x.OngId,
+                        principalTable: "Ongs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_SolicitacoesVoluntariado_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsuariosOngs",
+                columns: table => new
+                {
+                    OngsVoluntarioId = table.Column<int>(type: "int", nullable: false),
+                    VoluntariosId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsuariosOngs", x => new { x.OngsVoluntarioId, x.VoluntariosId });
+                    table.ForeignKey(
+                        name: "FK_UsuariosOngs_Ongs_OngsVoluntarioId",
+                        column: x => x.OngsVoluntarioId,
+                        principalTable: "Ongs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UsuariosOngs_Usuarios_VoluntariosId",
+                        column: x => x.VoluntariosId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SolicitacoesVoluntariado_OngId",
+                table: "SolicitacoesVoluntariado",
+                column: "OngId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SolicitacoesVoluntariado_UsuarioId",
+                table: "SolicitacoesVoluntariado",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsuariosOngs_VoluntariosId",
+                table: "UsuariosOngs",
+                column: "VoluntariosId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SolicitacoesVoluntariado");
+
+            migrationBuilder.DropTable(
+                name: "UsuariosOngs");
+
+            migrationBuilder.DropTable(
+                name: "Ongs");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
