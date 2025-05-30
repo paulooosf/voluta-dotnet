0ed1f2fd7454e024684a8ca4661c3dd924e8366a
Paulo Henrique <phpessoal2021@gmail.com>
2025-05-29 23:34:50 -0300

    feat: completando interface de serviço de Ong

--- Voluta/Services/IOngService.cs
+++ Voluta/Services/IOngService.cs
@@ -7,6 +7,9 @@ namespace Voluta.Services
     {
         Task<PaginatedViewModel<OngViewModel>> GetOngsAsync(int pagina, int tamanhoPagina);
         Task<OngViewModel> GetOngAsync(int id);
+        Task<OngViewModel> CreateOngAsync(NovaOngViewModel model);
+        Task<OngViewModel> UpdateOngAsync(int id, AtualizarOngViewModel model);
+        Task DeleteOngAsync(int id);
         Task<PaginatedViewModel<UsuarioViewModel>> GetVoluntariosDisponiveisAsync(int ongId, int pagina, int tamanhoPagina);
     }
 } 
